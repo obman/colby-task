5dc6f0ea5dd77b60b01480518c8497520fab2d2b
flygru <deniss9oo@gmail.com>
2025-07-29 11:28:38 +0200

    added products table + inserted mock product

--- docker/db.sql
+++ docker/db.sql
@@ -2,10 +2,22 @@ CREATE DATABASE IF NOT EXISTS colby;
 
 USE colby;
 
+CREATE TABLE IF NOT EXISTS products(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS media (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
     node_id VARCHAR(255) NOT NULL UNIQUE,
     type VARCHAR(255) NOT NULL,
     src TEXT,
-    position INT NOT NULL
+    position INT NOT NULL,
+    CONSTRAINT fk_product
+        FOREIGN KEY (product_id)
+            REFERENCES products(id)
+            ON DELETE CASCADE
 );
+
+INSERT INTO products (title) VALUE ("Resident Evil Village Pre-order (PC)");
